40f6bc4c97d33c9a53e9e0949f39d607c69986a1
codingcity.net <codingcity.net@gmail.com>
2021-02-03 15:22:28 +0900

    new instance

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'blwibzw)6r-bizc(vl3o%u@6l=3c0h72$xsky5h3=w8mog9bjn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.79.230.118']
+ALLOWED_HOSTS = ['3.226.226.82']
 
 
 # Application definition
