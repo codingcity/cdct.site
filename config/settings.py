08364f61521a97497256e3355f1bb63a7749b9bd
codingcity.net <codingcity.net@gmail.com>
2021-02-03 14:54:10 +0900

    allowd_hosts change

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'blwibzw)6r-bizc(vl3o%u@6l=3c0h72$xsky5h3=w8mog9bjn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.79.230.118']
 
 
 # Application definition
